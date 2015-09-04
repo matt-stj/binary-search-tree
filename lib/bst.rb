1eb8d5da6ee314d32e04f66719dbc977cfaf5542
Matt Stjernholm <thew88@gmail.com>
2015-09-03 20:53:37 -0600

    working for > case on root

--- lib/bst.rb
+++ lib/bst.rb
@@ -1,29 +1,32 @@
-require_relative 'null_node'
-require_relative 'node'
+require_relative 'null_node'  # => true
+require_relative 'node'       # => true
 
 class BinarySearchTree
 
-attr_accessor :root_node
+attr_accessor :root_node  # => nil
 
   def initialize(root_node = nil)
-    @root_node = root_node
-  end
+    @root_node = root_node         # => nil
+  end                              # => :initialize
 
   def insert(data)
-    if @root_node.nil?
-      @root_node = Node.new(data)
+    if @root_node.nil?                                         # => true, false
+      @root_node = Node.new(data)                              # => #<Node:0x007f8aaa80e260 @data="a", @left=nil, @right=nil>
+    elsif @root_node != nil
+      @root_node.left = Node.new(data) if data < @root_node.data
+      @root_node.right = Node.new(data) if data > @root_node.data
     end
   end
-    # elsif data < current_node
-    #   current_node = @root_node
-    #   until current_node.left.data
-    #
-    #   @root_node.left = current_position
-    #   until current_position.nil?
-    #
-    # else
-    #   @root_node.right
-    # end
+  #     current_node = @root_node                                # => #<Node:0x007f8aaa80e260 @data="a", @left=nil, @right=nil>
+  #     until current_node.left.nil? or current_node.right.nil?  # => true
+  #       if data < current_node.data
+  #         current_node.left = Node.new(data)
+  #       elsif data > current_node.data
+  #         current_node.right = Node.new(data)
+  #       end
+  #     end                                                      # => nil
+  #   end                                                        # => #<Node:0x007f8aaa80e260 @data="a", @left=nil, @right=nil>, nil
+  # end                                                          # => :insert
 
   # def search(data)
   #   return nil if root_node == nil
@@ -60,14 +63,15 @@ attr_accessor :root_node
   # end
 
 
-end
+end  # => :insert
 
-bst = BinarySearchTree.new
-bst.insert("a")
-bst
-bst.root_node.data
+bst = BinarySearchTree.new  # => #<BinarySearchTree:0x007f8aaa80e828 @root_node=nil>
+bst.insert("a")             # => #<Node:0x007f8aaa80e260 @data="a", @left=nil, @right=nil>
+bst.insert("b")             # => nil
+bst                         # => #<BinarySearchTree:0x007f8aaa80e828 @root_node=#<Node:0x007f8aaa80e260 @data="a", @left=nil, @right=nil>>
+bst.root_node.left          # => nil
 # bst.search("a")
-# bst.insert("b")
+
 # bst.insert("c")
 # bst.insert("d")
 # bst.insert("e")
