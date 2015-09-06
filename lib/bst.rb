e0f1c8fe4168382e76bd8b1fbc261469a14dc9a4
Matt Stjernholm <thew88@gmail.com>
2015-09-06 12:49:01 -0600

    working through depth - need to add more tests - max is broken

--- lib/bst.rb
+++ lib/bst.rb
@@ -18,7 +18,7 @@ attr_accessor :root_node
          while current_node != NullNode::DEFAULT
              if data < current_node.data && current_node.left == NullNode::DEFAULT
                  current_node.left = new_node
-             elsif  data > current_node.data && current_node.right == NullNode::DEFAULT
+             elsif data > current_node.data && current_node.right == NullNode::DEFAULT
                  current_node.right = new_node
              elsif data < current_node.data
                  current_node = current_node.left
@@ -31,11 +31,76 @@ attr_accessor :root_node
        end
    end
 
+
+   def include?(data)
+     current_node = @root_node
+     while current_node != NullNode::DEFAULT
+        if data == current_node.data
+          return true
+        elsif data < current_node.data && data != current_node.data
+          current_node = current_node.left
+        elsif data > current_node.data && data != current_node.data
+          current_node = current_node.right
+        end
+          if current_node == NullNode::DEFAULT
+            return false
+        end
+      end
+   end
+
+   def depth_of(data)
+     current_node = @root_node
+     depth = 0
+     while current_node != NullNode::DEFAULT
+        if data == current_node.data
+          return depth
+        elsif data < current_node.data && data != current_node.data
+          current_node = current_node.left
+          depth += 1
+        elsif data > current_node.data && data != current_node.data
+          current_node = current_node.right
+          depth += 1
+        end
+          if current_node == NullNode::DEFAULT
+            return depth
+        end
+      end
+   end
+
+   def maximum
+     current_node = @root_node
+     while current_node != NullNode::DEFAULT
+       maximum = current_node.data
+        if data == current_node.data
+          return maximum
+        elsif data < current_node.data && data != current_node.data
+          current_node = current_node.left
+          maximum = current_node.data if current_node.data > maximum
+        elsif data > current_node.data && data != current_node.data
+          current_node = current_node.right
+          maximum = current_node.data if current_node.data > maximum
+        end
+          if current_node == NullNode::DEFAULT
+            return depth
+        end
+      end
+   end
+
 end
 bst = BinarySearchTree.new
 bst.insert("d")
 bst
+bst.insert("c")
+bst
 bst.insert("b")
-bst
 bst.insert("a")
+bst.insert("f")
+bst.insert("g")
+bst.insert("h")
+bst.insert("k")
+
 bst
+bst.include?("a")
+bst.depth_of("f")
+bst
+bst.maximum
